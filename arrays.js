6fe54e47ba2403f8231f0af66cf74c122bfe89b6
FullStack2025 <19ru1a0553@ruce.ac.in>
2025-02-03 14:49:11 +0530

    added basics of javascript

---
+++ arrays.js
@@ -0,0 +1,16 @@
+let _3rdYear =[123,1234,12345,randomUser,randomUser1,randomUser2]
+let _3rdYear1 =[
+              {
+                name:"randomUser",
+                rollNo:"123"
+              },
+              {
+                name:"randomUser2",
+                rollNo:"1234"
+              },
+              {
+                name:"randomUser3",
+                rollNo:"12346"
+              },
+            ]
+console.log(array)
